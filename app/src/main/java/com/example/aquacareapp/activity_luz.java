b73f0b4583282b409a6f58521299b9bbc277ae19
Aqua-Care <zugscorp@gmail.com>
2022-10-31 22:51:06 -0300

    Mudança design e adição de telas 31/10/2022

---
+++ app/src/main/java/com/example/aquacareapp/activity_luz.java
@@ -0,0 +1,14 @@
+package com.example.aquacareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_luz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luz);
+    }
+}
