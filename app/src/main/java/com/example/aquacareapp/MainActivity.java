2f812132eb526dbe86bd60a84decdb4c096b9272
Luffev <lucasfernandesantunescorp@gmail.com>
2022-11-07 19:45:03 -0300

    Atualização 07/11/2022

--- app/src/main/java/com/example/aquacareapp/MainActivity.java
+++ app/src/main/java/com/example/aquacareapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.aquacareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btLuzMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLuzMain = findViewById(R.id.btLuzMain);
+
+        btLuzMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TelaLuz();
+            }
+        });
+    }
+
+    private void TelaLuz() {
+
+        startActivity(new Intent(this, activity_luz.class));
     }
 }
--- app/src/main/java/com/example/aquacareapp/activity_luz.java
+++ app/src/main/java/com/example/aquacareapp/activity_luz.java
@@ -3,12 +3,44 @@ package com.example.aquacareapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class activity_luz extends AppCompatActivity {
+
+    private Button btOnLuz;
+    private Button btOffLuz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luz);
+
+        btOnLuz = findViewById(R.id.btOnLuz);
+        btOffLuz = findViewById(R.id.btOffLuz);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+
+        btOnLuz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference Led = database.getReference("Led");
+                Led.setValue(1);
+            }
+        });
+
+        btOffLuz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference Led = database.getReference("Led");
+                Led.setValue(0);
+            }
+        });
     }
 }
