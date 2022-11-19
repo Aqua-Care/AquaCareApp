29a675c2dc8ced523655f5a131cf7ef7307fa098
Luffev <lucasfernandesantunescorp@gmail.com>
2022-11-19 16:24:54 -0300

    Atualização 19/11/2022

--- app/src/main/java/com/example/aquacareapp/activity_racao.java
+++ app/src/main/java/com/example/aquacareapp/activity_racao.java
@@ -1,11 +1,15 @@
 package com.example.aquacareapp;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +24,7 @@ public class activity_racao extends AppCompatActivity {
 
     private Button btAlimentoRacao;
     private TextView tvDataRacao;
+
     private DatabaseReference referencia;
 
 
@@ -34,8 +39,11 @@ public class activity_racao extends AppCompatActivity {
         btAlimentoRacao = findViewById(R.id.btAlimentoRacao);
         tvDataRacao = findViewById(R.id.tvDataRacao);
 
+
         referencia = FirebaseDatabase.getInstance().getReference("Usuarios")
                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Racao");
+
+
 
         btAlimentoRacao.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,20 +52,18 @@ public class activity_racao extends AppCompatActivity {
                 referencia.child("Alimentar").setValue(1);
 
                 try {
-                        Thread.sleep(2000);
+                    Thread.sleep(2000);
                 }
                 catch (InterruptedException e) {
-                        e.printStackTrace();
+                    e.printStackTrace();
                 }
 
                 Toast.makeText(activity_racao.this, "Uma porção de alimento foi oferecida", Toast.LENGTH_LONG).show();
                 referencia.child("Alimentar").setValue(0);
 
-                SimpleDateFormat formatoDataHora = new SimpleDateFormat("dd/mm/yyyy hh:mm:ss");
+                SimpleDateFormat formatoDataHora = new SimpleDateFormat("hh:mm:ss dd/mm/yyyy");
                 String data = formatoDataHora.format(new Date());
-                tvDataRacao.setText(data);
-
-
+                tvDataRacao.setText("Ultima Porção: " + data);
             }
         });
     }
