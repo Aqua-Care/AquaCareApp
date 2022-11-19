f6817cac160ad0338627a2931b3aab848c59a262
Luffev <lucasfernandesantunescorp@gmail.com>
2022-11-19 00:04:19 -0300

    Atualização 19/11/2022

--- app/src/main/java/com/example/aquacareapp/MainActivity.java
+++ app/src/main/java/com/example/aquacareapp/MainActivity.java
@@ -15,18 +15,37 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.aquacareapp.modelo.Usuario;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
+
     private Button btLuzMain;
     private Button btAguaMain;
     private Button btRacaoMain;
+
+    private TextView tvUser1Main;
+    //private TextView tvUser2Main;
+    //private TextView tvTelefoneMain;
+    //private TextView tvEmailMain;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String usuarioID;
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -52,17 +71,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-                    case R.id.item2:{
-                        Toast.makeText(MainActivity.this, "Contato", Toast.LENGTH_SHORT).show();
-                    }
+
                     case R.id.item4:{
-                        Toast.makeText(MainActivity.this, "Sair", Toast.LENGTH_SHORT).show();
+
+                        FirebaseAuth.getInstance().signOut();
+                        Toast.makeText(MainActivity.this, "Saiu", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, TourActivity.class));
                     }
                 }
 
                 return false;
             }
         });
+
+        ///////////////////////////////////////////////////////////////////////////////////
 
         btLuzMain = findViewById(R.id.btLuzMain2);
         btAguaMain = findViewById(R.id.btAguaMain);
@@ -87,6 +109,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, activity_agua.class));
+            }
+        });
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Usuarios");
+        usuarioID = user.getUid();
+
+        tvUser1Main = findViewById(R.id.tvUsuario1Main);
+        //tvUser2Main = findViewById(R.id.tvUsuario2Main);
+        //tvTelefoneMain = findViewById(R.id.tvTelefoneMain);
+        //tvEmailMain = findViewById(R.id.tvEmailMain);
+
+        reference.child(usuarioID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Usuario usuarioperfil = snapshot.getValue(Usuario.class);
+
+                if (usuarioperfil != null){
+
+                    String usuario1 = usuarioperfil.usuario;
+                    //String usuario2 = usuarioperfil.usuario;
+                    //String telefoneusuario = usuarioperfil.telefone;
+                    //String emailusuario = usuarioperfil.email;
+
+                    tvUser1Main.setText(usuario1);
+                    //tvUser2Main.setText(usuario2);
+                    //tvTelefoneMain.setText(telefoneusuario);
+                    //tvEmailMain.setText(emailusuario);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(MainActivity.this, "Algo errado aconteceu!", Toast.LENGTH_LONG).show();
             }
         });
     }
