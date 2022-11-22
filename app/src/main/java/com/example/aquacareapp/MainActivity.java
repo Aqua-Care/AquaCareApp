c62a34e1b773e6245894757a5b2e6809f9597de1
Luffev <lucasfernandesantunescorp@gmail.com>
2022-11-22 19:14:31 -0300

    Atualização 22/11/2022

--- app/src/main/java/com/example/aquacareapp/MainActivity.java
+++ app/src/main/java/com/example/aquacareapp/MainActivity.java
@@ -41,48 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference reference;
     private String usuarioID;
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(drawerToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
-    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-
-                    case R.id.item1:{
-
-                        FirebaseAuth.getInstance().signOut();
-                        Toast.makeText(MainActivity.this, "Saiu", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(MainActivity.this, TourActivity.class));
-                    }
-                    case  R.id.item2:{
-
-                        startActivity(new Intent(MainActivity.this, PerfilActivity.class));
-                    }
-                }
-
-                return false;
-            }
-        });
-
-        ///////////////////////////////////////////////////////////////////////////////////
 
         btLuzMain = findViewById(R.id.btLuzMain2);
         btAguaMain = findViewById(R.id.btAguaMain);
